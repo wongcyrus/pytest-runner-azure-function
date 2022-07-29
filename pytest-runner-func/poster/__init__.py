e165e61ded1e2c878fddb6a6305d6f7c0bf3b91c
Cyrus Wong <cywong@vtc.edu.hk>
2022-07-29 07:54:34 +0000

    add post support.

--- pytest-runner-func/poster/__init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-import azure.functions as func
-import json
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse(body=f"Success! Now please take the body back {req.get_body()}", status_code=200)
--- pytest-runner-func/pytester/__init__.py
+++ pytest-runner-func/pytester/__init__.py
@@ -1,3 +1,4 @@
+import imp
 import logging
 import subprocess
 import azure.functions as func
@@ -6,13 +7,29 @@ import tempfile
 import shutil
 import os
 import logging
+import json
 from pathlib import Path
+from urllib.parse import parse_qs
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("start")
-    source_code_file_path = req.params.get("sourceCodeFilePath")
-    source_code = req.params.get("sourceCode")
+    logging.info("start:" + req.method)
+
+    # Example
+    # sourceCodeFilePath = lab/lab01/ch01_t01_hello_world.py
+    # sourceCode = `print("Hello, world!")`
+
+    if req.method == "GET":
+        source_code_file_path = req.params.get("sourceCodeFilePath")
+        source_code = req.params.get("sourceCode")
+    else:
+        req_body_bytes = req.get_body()
+        logging.info(f"Request Bytes: {req_body_bytes}")
+        req_body = req_body_bytes.decode("utf-8")
+        logging.info(f"Request: {req_body}")
+        data = json.loads(req_body)
+        source_code_file_path = data["sourceCodeFilePath"]
+        source_code = data["sourceCode"]
 
     if not source_code or not source_code_file_path:
         return func.HttpResponse(body='{ "error": "source_code and source_code_file_path must present." }', status_code=422)
@@ -26,27 +43,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info("extractall")
 
         assignment_path = os.path.join(tmpdirname, "assignments")
+
+        code_file_path = os.path.join(
+            tmpdirname, "assignments", source_code_file_path)
+        with open(code_file_path, 'w') as filetowrite:
+            filetowrite.write(source_code)
+
+        text = Path(code_file_path).read_text()
+        logging.info(text)
+
+        # Source lab\lab01\ch01_t01_hello_world.py to Test tests\lab01\test_ch01_t01_hello_world.py
+        source_code_file_path_segments = source_code_file_path.split("/")
+        test_code_file_path = os.path.join(
+            tmpdirname, "assignments", "tests", source_code_file_path_segments[1], "test_"+source_code_file_path_segments[2])
+
         virtual_env_path = os.path.join(tmpdirname, "assignments", ".venv")
-        activate_virtual_env_path = os.path.join(tmpdirname, "assignments", ".venv","bin","activate")
-        pip_virtual_env_path = os.path.join(tmpdirname, "assignments", ".venv","bin","pip")
+        activate_virtual_env_path = os.path.join(
+            tmpdirname, "assignments", ".venv", "bin", "activate")
+        pip_virtual_env_path = os.path.join(
+            tmpdirname, "assignments", ".venv", "bin", "pip")
         os.chdir(assignment_path)
         test_result = subprocess.getoutput(
             f'python -m venv {virtual_env_path}')
         logging.info(test_result)
         test_result = subprocess.getoutput(
             f'{pip_virtual_env_path} install -r requirements.txt')
-        logging.info(test_result)          
-             
-        test_result_json = os.path.join(tmpdirname, 'result.json')        
+        logging.info(test_result)
+
+        test_result_json = os.path.join(tmpdirname, 'result.json')
         cmd = f""". {activate_virtual_env_path}
-python -m pytest --json-report --json-report-file={test_result_json}
+python -m pytest -v {test_code_file_path} --json-report --json-report-file={test_result_json}
 """
         test_result = subprocess.getoutput(cmd)
         logging.info(test_result)
         test_result_json = Path(test_result_json).read_text()
         logging.info(test_result_json)
         shutil.rmtree(tmpdirname)
-
-
 
     return func.HttpResponse(body=test_result_json, status_code=200)
